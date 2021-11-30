e993e92d708c4548422d2661e511dc40893db99d
hollowopk <1808479906@qq.com>
2021-11-30 16:11:39 +0800

    修复replay的bug，添加资源文件

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,12 @@
 import javafx.MainClient;
 import javafx.NIOClient;
+import javafx.RecordPlayer;
 import javafx.application.Application;
 
 public class Main {
     public static void main(String[] args) {
         //Application.launch(MainClient.class);
-        Application.launch(NIOClient.class);
+        //Application.launch(NIOClient.class);
+        Application.launch(RecordPlayer.class);
     }
 }
